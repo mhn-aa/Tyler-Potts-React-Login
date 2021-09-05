fe40bdaa1a38b2ccd9ff5c60c1609a785fe76778
mhn-aa <mahanmirjamali@gmail.com>
2021-09-05 13:31:53 +1000

    Failed Login

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import LoginForm from "./components/LoginForm";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const adminUser = {
+    email: "admin@admin.com",
+    password: "admin123",
+  };
+
+  const [user, setUser] = useState({ name: "", email: "" });
+  const [error, setError] = useState("");
+
+  const Login = (details) => {
+    console.log(details);
+  };
+
+  const Logout = () => {
+    console.log("Logout") ? (
+      <div className="welcome">
+        <h2>
+          Welcome, <span>{user.name}</span>
+        </h2>
+        <button>Logout</button>
+      </div>
+    ) : (
+      <LoginForm Login={Login} error={error} />
+    );
+  };
+
+  return <div className="App">{user.email !== ""}</div>;
 }
 
 export default App;
